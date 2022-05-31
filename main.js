5caf9213cdafd6f88ccfcf48e11e2f06dc972e4c
AyaanmandeliA <78735976+AyaanmandeliA@users.noreply.github.com>
2022-05-31 15:47:08 +0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,21 @@
+function about(){
+    window.location="About.html";
+}
+function action(){
+    window.location="action.html";
+}
+function exibition(){
+    window.location="exibition.html";
+}
+function pyp(){
+    window.location="pyp.html";
+}
+function reseach(){
+    window.location="research.html";
+}
+function interview(){
+    window.location="interview.html";
+}
+function bib(){
+    window.location="bib.html";
+}
